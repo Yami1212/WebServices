2d2abd6cbff912c1b88240c20465d37509b462e2
ivan <ivan.azamar@tesch.edu.mx>
2025-06-13 14:54:37 -0600

    EJEMPLO

--- app/src/main/java/com/example/webservices/MainActivity.java
+++ app/src/main/java/com/example/webservices/MainActivity.java
@@ -69,7 +69,7 @@ String url="http://172.16.1.12/services/login.php?u="+u+"&p="+p;
     }
     public void webServices2(View x){
     RequestQueue servicio2= Volley.newRequestQueue(this);
-        String u=e1.getText().toString();
+        String u=e1.getText().toString();//HOLA
         String p=e2.getText().toString();
         String url="http://10.29.179.188/backend_4951/index.php";
         StringRequest respuesta=new StringRequest(Request.Method.POST,
